1e237d904ea17adf558dfaff82c2b4b0cc8772da
SanyingYi <yisanyyy@gmail.com>
2023-12-07 16:40:54 +0800

    add function to read the sparse matrix from text file to the 2-dimensional array

--- src/lsh.c
+++ src/lsh.c
@@ -12,66 +12,112 @@
 const char *pathdata = "../data/test_matrix.txt";
 
 uint8_t shingle[DOCCOUNT][SHINGLECOUNT]; // read the doc-shingle 0-1 matrix to this matrix
-uint8_t sig[DOCCOUNT][HASHCOUNT];        // the signature matrix
+uint16_t sig[DOCCOUNT][HASHCOUNT];       // the signature matrix
 
-struct HashFamily hashfamily;
+// struct HashFamily hashfamily;
 
-struct HashTable checkedpairs;
+// struct HashTable checkedpairs;
 
 // int lshValidPairs = 0;
 
-// ==================== Core Minhash Function ====================
-void load_computeSig()
+// read the doc-shingle 0-1 matrix from the text file to shingle matrix
+void read_shingle_matrix()
 {
-    printf("Load and Compute Sig ...\n");
-
-    memset(sig, 0xff, sizeof(sig));
-    for (int i = 0; i < DOCCOUNT; i++)
-        for (int j = HASHCOUNT >> 1; j < HASHCOUNT; j++)
-            sig[i][j] = 0;
-
-    HashFamily_init(&hashfamily, HASHCOUNT >> 1);
-
-    FILE *fin = fopen(pathdata, "r");
-    for (int i = 0; i < DOCCOUNT; i++)
+    FILE *file = fopen(pathdata, "r");
+    if (file == NULL)
     {
-        int num;
-        char feature[16];
-        if (fscanf(fin, "%d%d", &i, &num) != 2)
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+    char line[DOCCOUNT * SHINGLECOUNT]; // each line has SHINGLECOUNT * 2 characters because of spaces
+    int row = 0, col = 0;               // index
+    // Read file line by line
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        col = 0;
+        char *token = strtok(line, " \n");
+        while (token != NULL)
         {
-            fprintf(stderr, "read format error!\n");
-            exit(1);
+            // printf("%d", atoi(token));
+            shingle[row][col] = (uint8_t)atoi(token);
+            token = strtok(NULL, " \n");
+            col++;
         }
+        row++;
 
-        for (int j = 0; j < num; j++)
+        // Check if the matrix is fully populated
+        if (row >= DOCCOUNT)
         {
-            if (fscanf(fin, "%s", feature) != 1 || strlen(feature) != 11)
-            {
-                fprintf(stderr, "read format error!\n");
-                exit(1);
-            }
-
-            int half = HASHCOUNT >> 1;
-            for (int k = 0; k < half; k++)
-            {
-                unsigned int res = HashFamily_hash(&hashfamily, k, feature, 11);
-                sig[i][k] = MIN(sig[i][k], res);
-                //				if(sig[i][k] > res) {
-                //					sig[i][k+half] = sig[i][k];
-                //					sig[i][k] = res;
-                //				}
-                //				else if(sig[i][k+half] > res)
-                //					sig[i][k+half] = res;
-                sig[i][k + half] = MAX(sig[i][k + half], res);
-            }
+            break;
         }
-
-        if (i && i % (DOCCOUNT / 10) == 0)
-            printf("Sig Complete:\t%.2lf%%\n", ((double)i) / DOCCOUNT);
     }
 
-    fclose(fin);
-    HashFamily_destroy(&hashfamily);
+    fclose(file);
+}
 
-    //	genCandbySig();
+// ==================== Core Minhash Function ====================
+// void load_computeSig()
+// {
+//     printf("Load and Compute Sig ...\n");
+
+//     memset(sig, 0xff, sizeof(sig));
+//     for (int i = 0; i < DOCCOUNT; i++)
+//         for (int j = HASHCOUNT >> 1; j < HASHCOUNT; j++)
+//             sig[i][j] = 0;
+
+//     HashFamily_init(&hashfamily, HASHCOUNT >> 1);
+
+//     FILE *fin = fopen(pathdata, "r");
+//     for (int i = 0; i < DOCCOUNT; i++)
+//     {
+//         int num;
+//         char feature[16];
+//         if (fscanf(fin, "%d%d", &i, &num) != 2)
+//         {
+//             fprintf(stderr, "read format error!\n");
+//             exit(1);
+//         }
+
+//         for (int j = 0; j < num; j++)
+//         {
+//             if (fscanf(fin, "%s", feature) != 1 || strlen(feature) != 11)
+//             {
+//                 fprintf(stderr, "read format error!\n");
+//                 exit(1);
+//             }
+
+//             int half = HASHCOUNT >> 1;
+//             for (int k = 0; k < half; k++)
+//             {
+//                 unsigned int res = HashFamily_hash(&hashfamily, k, feature, 11);
+//                 sig[i][k] = MIN(sig[i][k], res);
+//                 //				if(sig[i][k] > res) {
+//                 //					sig[i][k+half] = sig[i][k];
+//                 //					sig[i][k] = res;
+//                 //				}
+//                 //				else if(sig[i][k+half] > res)
+//                 //					sig[i][k+half] = res;
+//                 sig[i][k + half] = MAX(sig[i][k + half], res);
+//             }
+//         }
+
+//         if (i && i % (DOCCOUNT / 10) == 0)
+//             printf("Sig Complete:\t%.2lf%%\n", ((double)i) / DOCCOUNT);
+//     }
+
+//     fclose(fin);
+//     HashFamily_destroy(&hashfamily);
+
+//     //	genCandbySig();
+// }
+
+int main()
+{
+    read_shingle_matrix();
+    for (int i = 0; i < DOCCOUNT; i++)
+    {
+        for (int j = 0; j < SHINGLECOUNT; j++)
+            printf("%u", shingle[i][j]);
+        printf("\n");
+    }
 }
--- src/lsh.h
+++ src/lsh.h
@@ -3,9 +3,10 @@
 
 #include <stdint.h>
 
-#define DOCCOUNT 24000
-// #define DOCCOUNT 4
-#define SHINGLECOUNT 10232
+// #define DOCCOUNT 24000
+// #define SHINGLECOUNT 10232
+#define DOCCOUNT 4
+#define SHINGLECOUNT 4
 
 #define BANDCOUNT 50
 // #define BANDCOUNT 2
@@ -13,6 +14,8 @@
 #define HASHCOUNT (BANDCOUNT * LINEOFROWS)
 
 // #define THRESHOLD 0.83
+
+void read_shingle_matrix();
 
 void load_computeSig();
 
