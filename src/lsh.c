f63d157cc12c6c47c9d5ef85f57dbca24756a2d1
SanyingYi <yisanyyy@gmail.com>
2023-12-09 17:16:29 +0800

    added compute_LSH step and constructed a set-like data structure to store the candidate pairs

--- src/lsh.c
+++ src/lsh.c
@@ -1,27 +1,23 @@
 #include "lsh.h"
-// #include "hashfactory.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define MIN(a, b) (a < b ? a : b)
 #define MAX(a, b) (a > b ? a : b)
 
-// const char *pathdata = "../data/doc_shingle_matrix.txt";
-const char *pathdata = "../data/test_matrix.txt";
+const char *pathdata = "../data/doc_shingle_matrix.txt";
+// const char *pathdata = "../data/test_matrix.txt";
 
 uint32_t sig_hash_a[HASHCOUNT]; // the slope a of the signature hash functions
 uint32_t sig_hash_b[HASHCOUNT]; // the interception b of the signature hash functions
 
 uint8_t shingle[DOCCOUNT][SHINGLECOUNT]; // read the doc-shingle 0-1 matrix to this matrix
 uint16_t sig[DOCCOUNT][HASHCOUNT];       // the signature matrix
-
-// struct HashFamily hashfamily;
-
-// struct HashTable checkedpairs;
 
 // int lshValidPairs = 0;
 
@@ -63,7 +59,7 @@ void read_shingle_matrix()
     fclose(file);
 }
 
-// ====================Generate Hash Function Coefficients====================
+// ====================Generate Hash Function Coefficients for Minhash====================
 void generate_hash_function()
 {
     srand(time(NULL));
@@ -103,27 +99,110 @@ void compute_sig()
 }
 
 // ==================== Banded LSH Function to Generate Candidate Pairs====================
-void compute_LSH()
-{
-    for (int i = 0; i < BANDCOUNT; i++)
-    {
-        for (int j = 0; j < DOCCOUNT; j++)
-        {
+struct Tuple {
+    uint16_t first;
+    uint16_t second;
+};
 
-            char str[LINEOFROWS * 4 + 1];
-            str[LINEOFROWS * 4] = 'k';
-            for (int k = 0; k < LINEOFROWS; k++)
-                memcpy(str + k * 4, &sig[j][i * LINEOFROWS + k], 4);
-            assert(str[LINEOFROWS * 4] == 'k');
+struct Set {
+    struct Tuple *elements;
+    size_t size;
+    size_t capacity;
+};
 
-            // uint32_t hashval = HashFamily_hash(&hashfamily, i, str, LINEOFROWS * 4);
-            // Bucket_insert(hashval, j);
+struct Set candidatePairSet;
+
+// initialize a set
+void initializeSet(struct Set *set, size_t initialCapacity) {
+    set->elements = (struct Tuple *)malloc(initialCapacity * sizeof(struct Tuple));
+    if (set->elements == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+    set->size = 0;
+    set->capacity = initialCapacity;
+}
+
+// Function to check if a tuple is in the set
+bool isInSet(const struct Set *set, const struct Tuple *element) {
+    for (size_t i = 0; i < set->size; i++) {
+        if (set->elements[i].first == element->first && set->elements[i].second == element->second) {
+            return true;  // Tuple found in the set
         }
+    }
+    return false;  // Tuple not found in the set
+}
 
-        // Bucket_check(fout);
-        // Bucket_clear();
+// Function to resize the set
+void resizeSet(struct Set *set, size_t newCapacity) {
+    set->elements = (struct Tuple *)realloc(set->elements, newCapacity * sizeof(struct Tuple));
+    if (set->elements == NULL) {
+        perror("Memory reallocation failed");
+        exit(EXIT_FAILURE);
+    }
+    set->capacity = newCapacity;
+}
+
+// Function to add a tuple to the set
+void addToSet(struct Set *set, const struct Tuple *element) {
+    // Check if the tuple is already in the set
+    if (!isInSet(set, element)) {
+        // Add the tuple to the set
+        if (set->size == set->capacity) {
+            // Double the capacity (you can choose a different resizing strategy)
+            resizeSet(set, set->capacity * 2);
+        }
+        // Add the tuple to the set
+        set->elements[set->size++] = *element;
     }
 }
+
+// Function to print the elements of the set
+void printSet(const struct Set *set) {
+    printf("{ ");
+    for (size_t i = 0; i < set->size; i++) {
+        printf("(%d, %d) ", set->elements[i].first, set->elements[i].second);
+    }
+    printf("}\n");
+}
+
+// Function to free the memory allocated for the set
+void freeSet(struct Set *set) {
+    free(set->elements);
+    set->elements = NULL;
+    set->size = 0;
+    set->capacity = 0;
+}
+
+void compute_LSH()
+{
+    int flag=0;
+    // size_t initialCapacity = DOCCOUNT;
+    size_t initialCapacity = 10;
+    initializeSet(&candidatePairSet, initialCapacity);
+    for (int i = 0; i < BANDCOUNT; i++) // iterate through every band
+    {
+        for (int j = 0; j < DOCCOUNT-1; j++) // hash every sig. piece in the band to the bucket
+        {
+            for (int k = j+1; k<DOCCOUNT; k++){
+                flag = 0;
+                for (int r = 0; r < LINEOFROWS; r++){
+                    if (sig[j][i * LINEOFROWS + r] != sig[k][i * LINEOFROWS + r]){
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==0){
+                    // printf("%d, %d\n", j, k);
+                    struct Tuple candidate = {(uint16_t)j, (uint16_t)k};
+                    addToSet(&candidatePairSet, &candidate);
+                }
+            }
+        }
+    }
+}
+
+
 
 int main()
 {
@@ -140,13 +219,16 @@ int main()
     //     printf("%d+%d\n", sig_hash_a[i], sig_hash_b[i]);
     // }
     compute_sig();
-    printf("\nSig Matrix\n");
-    for (int i = 0; i < DOCCOUNT; i++)
-    {
-        for (int j = 0; j < HASHCOUNT; j++)
-        {
-            printf("%d ", sig[i][j]);
-        }
-        printf("\n");
-    }
+    // printf("\nSig Matrix\n");
+    // for (int i = 0; i < DOCCOUNT; i++)
+    // {
+    //     for (int j = 0; j < HASHCOUNT; j++)
+    //     {
+    //         printf("%d ", sig[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    compute_LSH();
+    printSet(&candidatePairSet);
+    freeSet(&candidatePairSet);
 }
--- src/lsh.h
+++ src/lsh.h
@@ -3,13 +3,13 @@
 
 #include <stdint.h>
 
-// #define DOCCOUNT 24000
-// #define SHINGLECOUNT 10232
-#define DOCCOUNT 4
-#define SHINGLECOUNT 4
+#define DOCCOUNT 24000
+#define SHINGLECOUNT 10232
+// #define DOCCOUNT 4
+// #define SHINGLECOUNT 4
 
-// #define BANDCOUNT 50
-#define BANDCOUNT 2
+#define BANDCOUNT 50
+// #define BANDCOUNT 2
 #define LINEOFROWS 2
 #define HASHCOUNT (BANDCOUNT * LINEOFROWS)
 
